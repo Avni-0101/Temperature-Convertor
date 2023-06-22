514618a288a241114d589813897c6bd7f3cabcd1
Avni Bansal <99727574+Avni-0101@users.noreply.github.com>
2023-06-22 15:12:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let data = document.querySelectorAll('.input-field');
 
-const celcuis = document.getElementById('celcius');
+const celsuis = document.getElementById('celsius');
 const fahrenheit = document.getElementById('fahrenheit');
 const kelvin = document.getElementById('kelvin');
 
@@ -16,18 +16,18 @@ for(let i=0; i<data.length;i++)
         let value = parseFloat(e.target.value);
         
         switch (e.target.id) {
-            case 'celcius':
+            case 'celsius':
                 fahrenheit.value = (value*1.8) +32;
                 kelvin.value = value + 273.15;    
 
                 break;
             case 'kelvin':
-                celcuis.value = value - 273.15;
+                celsuis.value = value - 273.15;
                 fahrenheit.value = (value - 273.15)*1.8 + 32;
                 
                 break;
             case 'fahrenheit':
-                celcuis.value = (value-32)/1.8;
+                celsuis.value = (value-32)/1.8;
                 kelvin.value = (value-32)/1.8 + 273.15;
                 
                 break;
